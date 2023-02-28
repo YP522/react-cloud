ec96ad68ad31431c4c478c2cdc5de76433aa1ad4
YP522 <yoannpetit22@free.fr>
2023-02-28 11:39:44 +0100

    :tada: Première livraison
    Initialisation du projet

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="flex flex-col h-screen">
+      {/* Header */}
+      <header className="bg-white shadow-sm">
+        <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-gray-900 text-xl font-bold">Mon site web</h1>
+        </div>
       </header>
+      
+      {/* Body */}
+      <main className="flex-1 bg-gray-100">
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          <div className="px-4 py-6 bg-white shadow-lg rounded-lg">
+            <h2 className="text-2xl font-bold text-gray-900 mb-4">Bienvenue sur mon site web</h2>
+            <p className="text-gray-700 leading-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu magna ut tortor 
+              eleifend euismod. Nulla interdum nibh nec turpis sodales, vel maximus eros convallis. 
+              Nunc iaculis mi sapien, a convallis arcu volutpat vel. Fusce id lorem risus. Sed mollis 
+              enim ac metus bibendum pharetra. Morbi at eleifend enim. Donec vel hendrerit sem. 
+            </p>
+          </div>
+        </div>
+      </main>
+
+      {/* Footer */}
+      <footer className="bg-white shadow-sm">
+        <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+          <p className="text-gray-700">© 2023 Mon site web. Tous droits réservés.</p>
+          <div>
+            <a href="#" className="text-blue-500 hover:text-blue-600 mr-6">Mentions légales</a>
+            <a href="#" className="text-blue-500 hover:text-blue-600">Contact</a>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
+
 
 export default App;
