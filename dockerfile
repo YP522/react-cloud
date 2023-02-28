bfdb0b671d0fb62a05a5c5804c64b3e44dacf736
YP522 <yoannpetit22@free.fr>
2023-02-28 14:26:52 +0100

    :tada: Seconde livraison

--- dockerfile
+++ dockerfile
@@ -1,14 +1,23 @@
-FROM node:13.12.0-alpine as build
+# Utiliser une image Node.js en tant que base
+FROM node:14.17.6-alpine3.14
+
+# Définir le répertoire de travail de l'application
 WORKDIR /app
-ENV PATH /app/node_modules/.bin:$PATH
-COPY package.json ./
-COPY package-lock.json ./
-RUN npm ci --silent
-RUN npm install react-scripts@3.4.1 -g --silent
-COPY . ./
+
+# Copier les fichiers package.json et package-lock.json dans le répertoire de travail
+COPY package*.json ./
+
+# Installer les dépendances
+RUN npm install --silent
+
+# Copier tous les fichiers du répertoire actuel dans le répertoire de travail de l'application
+COPY . .
+
+# Compiler l'application
 RUN npm run build
 
-FROM nginx:stable-alpine
-COPY --from=build /app/build /usr/share/nginx/html
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
+# Exposer le port sur lequel l'application écoute
+EXPOSE 3000
+
+# Définir la commande à exécuter pour lancer l'application
+CMD ["npm", "start"]
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import './index.css';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="flex flex-col h-screen">
+    <div className={`flex flex-col h-screen ${darkMode ? 'dark' : ''}`}>
       {/* Header */}
       <header className="bg-white shadow-sm">
-        <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
           <h1 className="text-gray-900 text-xl font-bold">Mon site web</h1>
+          <button
+            className="text-gray-700"
+            onClick={toggleDarkMode}
+          >
+            {darkMode ? 'Thème clair' : 'Thème sombre'}
+          </button>
         </div>
       </header>
       
       {/* Body */}
-      <main className="flex-1 bg-gray-100">
+      <main className="flex-1 bg-gray-100 dark:bg-gray-800">
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <div className="px-4 py-6 bg-white shadow-lg rounded-lg">
-            <h2 className="text-2xl font-bold text-gray-900 mb-4">Bienvenue sur mon site web</h2>
-            <p className="text-gray-700 leading-6">
+          <div className="px-4 py-6 bg-white dark:bg-gray-700 shadow-lg rounded-lg">
+            <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">Bienvenue sur mon site web</h2>
+            <p className="text-gray-700 dark:text-gray-300 leading-6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eu magna ut tortor 
               eleifend euismod. Nulla interdum nibh nec turpis sodales, vel maximus eros convallis. 
               Nunc iaculis mi sapien, a convallis arcu volutpat vel. Fusce id lorem risus. Sed mollis 
@@ -30,7 +42,7 @@ function App() {
       {/* Footer */}
       <footer className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-          <p className="text-gray-700">© 2023 Mon site web. Tous droits réservés.</p>
+          <p className="text-gray-700 dark:text-gray-300">© 2023 Mon site web. Tous droits réservés.</p>
           <div>
             <a href="#" className="text-blue-500 hover:text-blue-600 mr-6">Mentions légales</a>
             <a href="#" className="text-blue-500 hover:text-blue-600">Contact</a>
@@ -40,6 +52,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
